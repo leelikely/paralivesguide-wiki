d8e159e570abd71be5a4540a6f3f517d6ba5d6ad
leelikely <506995883@qq.com>
2026-05-22 20:32:56 +0800

    Add files via upload

--- paralivesguide.wiki/sw.js
+++ paralivesguide.wiki/sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 11042084
+    "zoneId": 11042123
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
